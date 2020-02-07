c1cd7c463d19e3545b33b1b39ba114fa8a56230e
哆啦A梦 <hg9558@126.com>
2020-02-07 23:12:34 +0800

    修改侧边栏央视u

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,14 +3,14 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _df312f4a = () => interopDefault(import('..\\pages\\404.vue' /* webpackChunkName: "pages_404" */))
-const _4bab0a0d = () => interopDefault(import('..\\pages\\example.vue' /* webpackChunkName: "pages_example" */))
-const _910d3944 = () => interopDefault(import('..\\pages\\video.vue' /* webpackChunkName: "pages_video" */))
-const _7ae78ee1 = () => interopDefault(import('..\\pages\\user\\login.vue' /* webpackChunkName: "pages_user_login" */))
-const _46fd125b = () => interopDefault(import('..\\pages\\user\\register.vue' /* webpackChunkName: "pages_user_register" */))
-const _3da0afe2 = () => interopDefault(import('..\\pages\\user\\resetpassword.vue' /* webpackChunkName: "pages_user_resetpassword" */))
-const _f47b889e = () => interopDefault(import('..\\pages\\user\\userinfo.vue' /* webpackChunkName: "pages_user_userinfo" */))
-const _37e47435 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+const _0f30bf78 = () => interopDefault(import('..\\pages\\404.vue' /* webpackChunkName: "pages_404" */))
+const _09898aaa = () => interopDefault(import('..\\pages\\example.vue' /* webpackChunkName: "pages_example" */))
+const _ad6d318a = () => interopDefault(import('..\\pages\\video.vue' /* webpackChunkName: "pages_video" */))
+const _2e2a78fe = () => interopDefault(import('..\\pages\\user\\login.vue' /* webpackChunkName: "pages_user_login" */))
+const _1ee7171e = () => interopDefault(import('..\\pages\\user\\register.vue' /* webpackChunkName: "pages_user_register" */))
+const _2fcba602 = () => interopDefault(import('..\\pages\\user\\resetpassword.vue' /* webpackChunkName: "pages_user_resetpassword" */))
+const _5dac4074 = () => interopDefault(import('..\\pages\\user\\userinfo.vue' /* webpackChunkName: "pages_user_userinfo" */))
+const _29b47812 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -30,39 +30,39 @@ export const routerOptions = {
 
   routes: [{
     path: "/404",
-    component: _df312f4a,
+    component: _0f30bf78,
     name: "404"
   }, {
     path: "/example",
-    component: _4bab0a0d,
+    component: _09898aaa,
     name: "example"
   }, {
     path: "/video",
-    component: _910d3944,
+    component: _ad6d318a,
     name: "video"
   }, {
     path: "/user/login",
-    component: _7ae78ee1,
+    component: _2e2a78fe,
     name: "user-login"
   }, {
     path: "/user/register",
-    component: _46fd125b,
+    component: _1ee7171e,
     name: "user-register"
   }, {
     path: "/user/resetpassword",
-    component: _3da0afe2,
+    component: _2fcba602,
     name: "user-resetpassword"
   }, {
     path: "/user/userinfo",
-    component: _f47b889e,
+    component: _5dac4074,
     name: "user-userinfo"
   }, {
     path: "/",
-    component: _37e47435,
+    component: _29b47812,
     name: "index"
   }, {
     path: "*",
-    component: _df312f4a,
+    component: _0f30bf78,
     name: "custom"
   }],
 
