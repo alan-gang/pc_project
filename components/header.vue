6d73264c071463cba765f5aa477b8d16493ef451
alan <hg9558@126.com>
2020-02-08 18:59:58 +0800

    123

--- components/header.vue
+++ components/header.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-    头部组件
+  <div class="header-container c_f">
+    <div class="side-controller">
+      <i :class="isSlider?'el-icon-s-unfold':'el-icon-s-fold'"
+         class="ml_20 ft_24 c_r"
+         @click="changeSlider(isSlider= !isSlider)"></i>
+    </div>
+    <div class="header-main">头部文件</div>
   </div>
 </template>
 
 <script>
-  export default {
-
+import { mapMutations } from 'vuex'
+export default {
+  data () {
+    return {
+      isSlider: false
+    }
+  },
+  created () {
+    console.log(this.$store._mutations)
+  },
+  methods: {
+    // ...mapMutations({
+    //   changeSlider: this.$store.mutations.home.changeSlider
+    // })
   }
+}
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+  .header-container
+      background-color: #141619;
+      height 60px
+      flex(flex-start)
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,8 @@
 <template>
   <el-container class="main-container">
-    <el-aside  width="200px">
-      <el-menu default-active="1"
-               class="asside-container"
-               @open="handleOpen"
-               @close="handleClose"
-               :collapse="isCollapse">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <el-menu-item index="1-2">选项3</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-        </el-menu>
-    </el-aside>
+    <m-sidebar></m-sidebar>
     <el-container>
-      <el-header>Header</el-header>
+      <m-header></m-header>
       <el-main>
         <nuxt />
       </el-main>
@@ -43,19 +12,20 @@
 
 <script>
 
+import mHeader from '~/components/header'
+import mSidebar from '~/components/sidebar'
 export default {
+  components:{
+    mHeader,
+    mSidebar
+  },
   data () {
     return {
-      isCollapse: false
+
     }
   },
   methods: {
-    handleOpen () {
 
-    },
-    handleClose () {
-
-    }
   }
 }
 </script>
@@ -64,20 +34,6 @@ export default {
   .main-container
     height 100%
     display flex
-    .el-header
-      background-color: #B3C0D1;
-      color: #333;
-      text-align: center;
-      line-height: 60px;
-    .el-aside
-      background-color: #D3DCE6;
-      color: #333;
-      text-align: center;
-      line-height: 200px;
-    .asside-container
-      height 100%
-      line-height 1
-      background-color orange
     .el-main
       background-color: #E9EEF3;
       color: #333;
