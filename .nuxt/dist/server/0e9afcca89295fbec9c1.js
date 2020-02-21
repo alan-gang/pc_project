b2321af55984cc0b781821f8a2fa85de3220c235
alan <hg9558@126.com>
2020-02-21 15:13:26 +0800

    添加用户基本模块

---
+++ .nuxt/dist/server/0e9afcca89295fbec9c1.js
@@ -0,0 +1,1 @@
+exports.ids=[10],exports.modules={130:function(t,e,r){"use strict";r.r(e);var n={},o=r(1);var component=Object(o.a)(n,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._ssrNode("\n  用户详情\n")])}),[],!1,(function(t){}),"f23b2674","3678d770");e.default=component.exports}};
