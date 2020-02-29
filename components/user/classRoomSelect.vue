e0589551284d6f1bf3e18e19c543bd16d1d00d48
哆啦A梦 <hg9558@126.com>
2020-02-29 21:23:10 +0800

    重新打包

---
+++ components/user/classRoomSelect.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-cascader ref="className" class="ml_20"  :options="classOptions" readonly :placeholder="`${form.className?form.className:'请输入你的班级/专业'}`" v-model="form.className" @change=" _updateUser('className',{className:$refs['className'].getCheckedNodes()[0].pathLabels.join('/')})"></el-cascader>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+</style>
